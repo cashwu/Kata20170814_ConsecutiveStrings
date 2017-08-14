024171b9b832638b7e6d27f1ba06dafabd6f1ae5
cash <cash@cashwu.com>
2017-08-14 20:55:17 +0800

    input a, b and 0 test case

--- Kata20170814_ConsecutiveStrings/ConsecutiveStringsTests.cs
+++ Kata20170814_ConsecutiveStrings/ConsecutiveStringsTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170814_ConsecutiveStrings
     public class ConsecutiveStringsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_a_b_and_0_should_return_empty()
         {
+            LongestConsecShouldBe("", new[] { "a", "b" }, 0);
+        }
+
+        private static void LongestConsecShouldBe(string expected, string[] strarr, int count)
+        {
+            var longest = new LongestConsecutives();
+            var actual = longest.LongestConsec(strarr, count);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class LongestConsecutives
+    {
+        public string LongestConsec(string[] strarr, int k)
+        {
+            return "";
         }
     }
 }
