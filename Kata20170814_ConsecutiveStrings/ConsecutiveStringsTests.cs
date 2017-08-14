45f7a1e97a7e47402b77480367f816e1759e457e
cash <cash@cashwu.com>
2017-08-14 20:47:19 +0800

    init

--- Kata20170814_ConsecutiveStrings/ConsecutiveStringsTests.cs
+++ Kata20170814_ConsecutiveStrings/ConsecutiveStringsTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170814_ConsecutiveStrings
 {
     [TestClass]
-    public class UnitTest1
+    public class ConsecutiveStringsTests
     {
         [TestMethod]
         public void TestMethod1()
